b41686f76956e326afa8d76f3d5a94500be9775a
bhupeshg2 <bhupesh00gupta@gmail.com>
2015-01-22 15:02:13 +0530

    initial commit

---
+++ transportion_management/config/desktop.py
@@ -0,0 +1,11 @@
+from frappe import _
+
+def get_data():
+	return {
+		"Transportation Management": {
+			"color": "#589494",
+			"icon": "icon-truck",
+			"type": "module",
+			"label": _("Transportation Management")
+		}
+	}
---
+++ transportion_management/hooks.py
@@ -0,0 +1,107 @@
+app_name = "transportion_management"
+app_title = "Transportation Management"
+app_publisher = "Arun Logistics"
+app_description = "An app to manage transportation vehicles, keep record of transfers, advances, PF, fule provided"
+app_icon = "icon-truck"
+app_color = "#589494"
+app_email = "bhupesh00gupta@gmail.com"
+app_url = "https://frappe.io/apps/transport"
+app_version = "0.0.1"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/transportion_management/css/transportion_management.css"
+# app_include_js = "/assets/transportion_management/js/transportion_management.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/transportion_management/css/transportion_management.css"
+# web_include_js = "/assets/transportion_management/js/transportion_management.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "transportion_management.install.before_install"
+# after_install = "transportion_management.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "transportion_management.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.core.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.core.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"transportion_management.tasks.all"
+# 	],
+# 	"daily": [
+# 		"transportion_management.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"transportion_management.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"transportion_management.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"transportion_management.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "transportion_management.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.core.doctype.event.event.get_events": "transportion_management.event.get_events"
+# }
+
